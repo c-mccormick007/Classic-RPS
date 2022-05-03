b8afc8d4655edda0e69fbace612913dbdc6180c1
c-mccormick007 <c.mccormick1295@gmail.com>
2022-05-02 19:39:23 -0500

    add styles to the website

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ let result = "";
 for (let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', () => {
         result = playRound(buttons[i].value,computerPlay());
-        console.log(result);
     });
 }
 /* function game(){
